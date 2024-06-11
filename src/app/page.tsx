24662d5890d8a5c745e0df40d215ad544e2bbe54
OprahMB <ohuang@omnisolu.com>
2024-06-11 16:32:04 -0700

    fix: deleted the fail code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,20 +7,16 @@ import PhantomWallet from './components/PhantomWallet'
 import AppWalletProvider from "./components/AppWalletProvider"
 import WalletAirdrop from "@/app/address/page"
 import UnifiedWalletKit from "@/app/components/UnifiedWalletKit"
-import ChainStack from "@/app/sendTransaction/page"
+import ChainStack from "@/app/chaintask/page"
 
 export default function Home() {
   return (
     <main className="w-full py-24 min-h-screen bg-[#F4F2FE] text-center">
-      <p>Solana Wallet Adapter:</p>
-      <AppWalletProvider />
       <div className="space-y-2">
         <p>Unified Wallet Adapter:</p>
         <UnifiedWalletKit />
       </div>
-      <ConnectPhantom />
       <PhantomWallet />
-      <ChainStack />
     </main>
   );
 }
